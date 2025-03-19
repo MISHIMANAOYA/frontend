ddb4b9dbd63a2772acc3862cdeb0de3f48b44056
Naoya Mishima <mishima.naoya@jp.panasonic.com>
2025-03-19 10:02:03 +0900

    フロントエンド環境構築

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = useState(null);
+
+    useEffect(() => {
+      fetch('http://127.0.0.1:5000/api/data')
+          .then(response => response.json())
+          .then(data => setData(data))
+          .catch(error => console.error('Error:', error));
+  }, []);
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>{data ? data.message : "Loading..."}</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
